8e46958fcca382d193684363b5b9d3eb4481eb2b
InnaVv <2715831byyfBYYF>
2020-11-15 23:17:14 +0300

    Added Test data and cycle

--- mail2/EmailPage.java
+++ mail2/EmailPage.java
@@ -5,29 +5,22 @@ import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
+import java.util.List;
+
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class EmailPage extends LoginPage {
-    SelenideElement newestEmailinTheList = $(By.xpath("//div[contains(@class, 'ns-view-messages-item-wrap')][1]//span[contains(@class,'MessageSnippet-Item_subject')]/span[contains(@class,'MessageSnippet-Item_subject')]"));
-    SelenideElement refreshButton = $(By.cssSelector(".mail-ComposeButton-Refresh"));
-    SelenideElement sentMailLink = $(By.xpath("//a[@href=\"#sent\"]"));
-    SelenideElement sentMailFirst = $(By.xpath("/descendant::div[contains(@class, 'mail-MessageSnippet-Content')][1]"));
 
     SelenideElement logoUser = $(By.cssSelector(".legouser__current-account"));
     SelenideElement logoutButton = $(By.xpath("//span[contains(text(),'Выйти из сервисов Яндекса')]"));
 
-    public String lookForMessage() {
-        String result = null;
+    public List<String> lookForMessageSubject() {
+        List <String> result = null;
         ElementsCollection sentMailList = $$(By.xpath("/descendant::div[contains(@class, 'mail-MessageSnippet-Content')]"));
-        SelenideElement actualSubjectValue = $(By.cssSelector(".mail-Message-Toolbar-Subject"));
 
         for (SelenideElement mailSubject : sentMailList) {
-//            mailSubject.click();
-//            actualSubjectValue.waitUntil(Condition.appear, 6000);
-            result = mailSubject.find(By.cssSelector(".mail-MessageSnippet-Item_sender")).getText();
-
-//            result = mailSubject.find(By.cssSelector(".mail-MessageSnippet-FromText")).getText();
+            result.add(mailSubject.find(By.cssSelector(".mail-MessageSnippet-FromText")).getAttribute("title")) ;
         }
         return result;
     }
--- mail2/EmailTest.java
+++ mail2/EmailTest.java
@@ -2,10 +2,10 @@ package com.sparta.mail2;
 
 import com.github.javafaker.Faker;
 import org.testng.Assert;
+import org.testng.AssertJUnit;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
 
 import static com.codeborne.selenide.WebDriverRunner.clearBrowserCache;
 
@@ -15,7 +15,6 @@ public class EmailTest {
     EmailPage emailPage = new EmailPage();
     MessagePage messagePage = new MessagePage();
     Faker faker = new Faker();
-    SoftAssert softAssert = new SoftAssert();
 
     @BeforeMethod
     public void clearCache() {
@@ -35,9 +34,7 @@ public class EmailTest {
 
         emailPage.login(credentialsData.getUrl(),credentialsData.getMail(),credentialsData.getPass());
         messagePage.sendEmail(credentialsData.getRecipient(), subject, body);
-//        Assert.assertTrue(emailPage.lookForMessage(subject), "There is NO email with such subject");
+        Assert.assertTrue(emailPage.lookForMessageSubject().contains("a4testtest@yandex.ru"), "There is NO email with such recepient");
 
-        softAssert.assertEquals(emailPage.lookForMessage(), "a2testtest@yandex.ru", "There is NO email with such subject");
-        softAssert.assertAll();
     }
 }
